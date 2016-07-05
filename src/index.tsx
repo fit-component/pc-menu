7c9e9012d20300a980153f78e200464a4b0c8dd7
huangziyi01 <huangziyi01@baidu.com>
2016-07-05 10:07:48 +0800

    quick push

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 /// <reference path="../../../../typings/tsd.d.ts" />
-/// <reference path="../../../../typings-module/fit-style.d.ts" />
 import Menu from './menu'
 import MenuItem from './menu-item'
 import SubMenu from './sub-menu'
