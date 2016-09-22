ce8a75b2e81e0125b97196651337075f847f1dc2
Qi_Lyu <huashiyiqike@qq.com>
2016-09-22 10:19:41 +0800

    quick push

--- demo/index.js
+++ demo/index.js
@@ -1,2 +1,2 @@
 // @demo
-// basic:24 reverse:24 right:24 vertical:24 sub-tree:24 sub-tree-vertical:24
+// basic:24 reverse:24 right:24 vertical:24 sub-tree:24 sub-tree-vertical:24 right-sub-tree:24
--- demo/lists/right-sub-tree.jsx
+++ demo/lists/right-sub-tree.jsx
@@ -5,7 +5,7 @@ export default class Basic extends React.Component<any, any> {
     render() {
         return (
             <Menu height={100}>
-                <MenuItem brand>普通菜单</MenuItem>
+                <MenuItem brand>靠右子菜单</MenuItem>
                 <MenuItem>普通菜单</MenuItem>
                 <RightMenu>
                     <SubMenu title="菜单">
